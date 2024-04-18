49681e5f1c7abcf1aa431321a3612600a20f8d7a
NitratoTri <148335354+NitratoTri@users.noreply.github.com>
2024-04-18 19:42:29 +0200

    -Added resources folder with background image,changes in loguin fxml

--- Pokemon/src/login/loginController.java
+++ Pokemon/src/login/loginController.java
@@ -7,4 +7,10 @@ public class loginController {
 	public void register(ActionEvent e) {
 		System.out.println("Registrado maquina");
 	}
+	public void login(ActionEvent e) {
+		System.out.println("Logueado maquina");
+	}
+	public void close(ActionEvent e) {
+		System.out.println("Cerrado maquina");
+	}
 }
