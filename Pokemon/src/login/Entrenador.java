0372958e0943e3b150739fd5ff810271fbf620e3
NitratoTri <148335354+NitratoTri@users.noreply.github.com>
2024-04-22 18:45:18 +0200

    -Jeje

---
+++ Pokemon/src/login/Entrenador.java
@@ -0,0 +1,105 @@
+package login;
+
+import java.util.LinkedList;
+
+public class Entrenador {
+	private int idEntrador;
+	private String nombre;
+	private String pass;
+	private double pokedollares;
+	LinkedList<Pokemon> equipoPrincipal;
+	LinkedList<Pokemon> pokemonCaja;
+	
+	
+	public Entrenador(int idEntrador, String nombre, String pass, double pokedollares,
+			LinkedList<Pokemon> equipoPrincipal, LinkedList<Pokemon> pokemonCaja) {
+		super();
+		this.idEntrador = idEntrador;
+		this.nombre = nombre;
+		this.pass = pass;
+		this.pokedollares = pokedollares;
+		this.equipoPrincipal = equipoPrincipal;
+		this.pokemonCaja = pokemonCaja;
+	}
+
+
+	public Entrenador(String nombre, String pass) {
+		this.idEntrador = 0;
+		this.nombre = nombre;
+		this.pass = pass;
+		this.pokedollares = 0;
+		this.equipoPrincipal = new LinkedList<Pokemon>();
+		this.pokemonCaja = new LinkedList<Pokemon>();
+	}
+
+
+	public int getIdEntrador() {
+		return idEntrador;
+	}
+
+
+	public void setIdEntrador(int idEntrador) {
+		this.idEntrador = idEntrador;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getPass() {
+		return pass;
+	}
+
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+
+	public double getPokedollares() {
+		return pokedollares;
+	}
+
+
+	public void setPokedollares(double pokedollares) {
+		this.pokedollares = pokedollares;
+	}
+
+
+	public LinkedList<Pokemon> getEquipoPrincipal() {
+		return equipoPrincipal;
+	}
+
+
+	public void setEquipoPrincipal(LinkedList<Pokemon> equipoPrincipal) {
+		this.equipoPrincipal = equipoPrincipal;
+	}
+
+
+	public LinkedList<Pokemon> getPokemonCaja() {
+		return pokemonCaja;
+	}
+
+
+	public void setPokemonCaja(LinkedList<Pokemon> pokemonCaja) {
+		this.pokemonCaja = pokemonCaja;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Entrenador [idEntrador=" + idEntrador + ", nombre=" + nombre + ", pass=" + pass + ", pokedollares="
+				+ pokedollares + ", equipoPrincipal=" + equipoPrincipal + ", pokemonCaja=" + pokemonCaja + "]";
+	}
+	
+	
+	
+	
+}
--- Pokemon/src/login/Main.java
+++ Pokemon/src/login/Main.java
@@ -1,5 +1,9 @@
 package login;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,7 +14,9 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage)  {
-		// TODO Auto-generated method stub
+		
+
+	
 		 	try {
 		 		Parent root= FXMLLoader.load(getClass().getResource("loguin.fxml"));
 		 		Scene scene =new Scene(root);
@@ -20,6 +26,31 @@ public class Main extends Application {
 		 	}catch(Exception e) {
 		 		e.printStackTrace();
 		 	}
+		 	
+		 	Connection con = null;
+			String url = "jdbc:mysql://localhost:3306/pokemon";
+			String login = "root";
+			String password = "";
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				con = DriverManager.getConnection(url, login, password);
+				System.out.println("Conexión establecida");
+
+				Entrenador entrenador = new Entrenador("LUIS", "123456");
+				
+				
+				
+				System.out.println(entrenador.toString());
+			} catch (SQLException | ClassNotFoundException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					con.close();
+					System.out.println("Conexión cerrada");
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 	}
 
 }
